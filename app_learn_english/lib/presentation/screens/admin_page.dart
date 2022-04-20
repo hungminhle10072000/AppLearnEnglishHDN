da510b6f2aee20f9b62bdc99ea49d51b77e175d9
= <=>
2022-04-20 21:38:38 +0700

    test-save-SharedPreferences

--- app_learn_english/lib/presentation/screens/admin_page.dart
+++ app_learn_english/lib/presentation/screens/admin_page.dart
@@ -1,12 +1,48 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class adminPage extends StatelessWidget {
+class adminPage extends StatefulWidget {
   const adminPage({Key? key}) : super(key: key);
+
+  @override
+  _adminPageState createState() => _adminPageState();
+}
+
+class _adminPageState extends State<adminPage> {
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  var fullname = "";
+  String username = "";
+
+
+  @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+
+  void getData() async{
+    SharedPreferences pref = await SharedPreferences.getInstance();
+
+    setState(() {
+      fullname = pref.getString('fullname')!;
+      username = pref.getString('username')!;
+    });
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Admin page"),
+        child:
+        //Text("Admin page"),
+        Column(
+            children: [
+              Text("Admin page  ${fullname}"),
+              SizedBox(height: 30.0),
+              Text("Admin page ${username}"),
+            ])
     );
   }
 }
