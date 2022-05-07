472b14c48205c894ae73842f121c0b4bc753a0a6
HaDucDev <haductrung392000@gmail.com>
2022-05-07 23:09:18 +0700

    link run ok

--- app_learn_english/lib/utils/constants/Cons.dart
+++ app_learn_english/lib/utils/constants/Cons.dart
@@ -1,5 +1,5 @@
-const String baseUrl = "https://be-app-learn-english.herokuapp.com";
-// const String baseUrl = "https://0b3b-27-74-123-102.ap.ngrok.io";
+//const String baseUrl = "https://be-app-learn-english.herokuapp.com";
+ const String baseUrl = "https://516c-171-232-236-246.ap.ngrok.io";
 class fontSize {
   static const double small = 16;
   static const double medium = 20;
